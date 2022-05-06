30b0d244ec235cb8044b9e3de9ef83b88dd15acf
Ben Jackson <ben.d.jackson@outlook.com>
2022-05-06 21:02:01 +0100

    Fixing deleting CNAME

--- scripts/clean.js
+++ scripts/clean.js
@@ -3,5 +3,7 @@ const upath = require('upath');
 
 const destPath = upath.resolve(upath.dirname(__filename), '../docs');
 
-sh.rm('-rf', `${destPath}/*`)
-
+sh.rm('-rf', `${destPath}/index.html`)
+sh.rm('-rf', `${destPath}/assets`)
+sh.rm('-rf', `${destPath}/css`)
+sh.rm('-rf', `${destPath}/js`)
